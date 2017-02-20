9b1f11b60e05d26915b249c014ce7fc00cc6abcd
Sergey Zelenov <sergey.zelenov@zalando.de>
2017-02-21 00:10:31 +0100

    move formating of body directly to app and add method for fetching user

--- modules/app.js
+++ modules/app.js
@@ -1,12 +1,19 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const {fetchUsersByLanguage} = require('./githubClient');
+const formatUser = require('../utils/formatUser');
 
 const app = module.exports = new Koa();
 const router = new Router({
   prefix: '/api'
 });
 
-router.get('/users/:language', fetchUsersByLanguage);
+router.get('/users/:language', async ctx => {
+  const response = await fetchUsersByLanguage(ctx.params.language);
+
+  ctx.body = {
+    items: response.items.map(formatUser)
+  };
+});
 
 app.use(router.routes());
--- modules/githubClient.js
+++ modules/githubClient.js
@@ -4,12 +4,11 @@ const request = require('request-promise').defaults({
 });
 const url = require('url');
 const qs = require('qs');
-const formatUser = require('../utils/formatUser');
 
-exports.fetchUsersByLanguage = async ctx => {
+exports.fetchUsersByLanguage = async language => {
   const query = Object.assign({},
     config.get('github.client.query'),
-    {q: `language:${ctx.params.language}`}
+    {q: `language:${language}`}
   );
 
   const uri = url.format({
@@ -24,7 +23,25 @@ exports.fetchUsersByLanguage = async ctx => {
     headers: config.get('github.client.headers')
   });
 
-  ctx.body = {
-    items: response.items.map(formatUser)
-  };
+  return response;
+};
+
+exports.fetchUser = async username => {
+  const query = Object.assign({},
+    config.get('github.client.query')
+  );
+
+  const uri = url.format({
+    protocol: config.get('github.client.protocol'),
+    host: config.get('github.client.host'),
+    pathname: `/users/${username}`,
+    search: qs.stringify(query, {encode: false})
+  });
+
+  const response = await request({
+    uri,
+    headers: config.get('github.client.headers')
+  });
+
+  return response;
 };
